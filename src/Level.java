e16149f2751dbd94df0569d467487bb0ba8ca71d
Pascal <pascal@fornal.eu>
2019-02-18 14:30:34 +0100

    in

---
+++ src/Level.java
@@ -0,0 +1,57 @@
+public class Level {
+    private int lvl;
+    private int field[][];
+
+    public Level(int lvl) {
+        this.lvl = lvl;
+        this.initLVL();
+    }
+
+    private void initLVL() {
+        switch (this.lvl) {
+            case 1:
+                System.out.println("one");
+                this.field=new int[][] {
+                {0, 8, 0, 0, 0},
+                {0, 0, 0, 0, 0, 8},
+                {0, 0, 0, 8, 0, 0, 0},
+                {8, 0, 8, 0, 0, 0, 0, 8},
+                {0, 0, 0, 0, 8, 0, 0, 0, 0},
+                {8, 0, 0, 0, 0, 8, 0, 0},
+                {0, 0, 8, 8, 0, 0, 8},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0, 8, 0, 0}
+            };
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+        }
+    }
+
+    public void rotateLVL()
+    {
+
+    }
+
+    //____Log functions___
+    public void printLVL() {
+        for (int[] line : this.field) {
+            System.out.println();
+            for(int i=0 ; i<(9-line.length); i++)
+            {
+                System.out.print(" ");
+            }
+             for (int field: line)
+             {
+                 System.out.print(" "+field);
+             }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Level LVLone = new Level(1);
+        LVLone.printLVL();
+
+    }
+}
