d033e6700c733d66b533f34f722b982809df1e2b
Pascal <pascal@fornal.eu>
2019-04-18 15:20:46 +0200

    testing all

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import DeterminedTesting.DetMCTS_ClassicE_SmartR;
 import DeterminedTesting.DetMCTS_SmartE_AllR;
 import DeterminedTesting.DetMCTS_SmartE_SmartR;
 import GameSession.*;
+import StandardTesting.*;
 import output.Output;
 
 import java.util.ArrayList;
@@ -13,33 +14,136 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+/**
+ * Determined Test
+ */
+//        Output.getInstance().writeToFile("Player One:DetMCTS_ClassicE_SmartR || Player Two: DetMCTS_SmartE_AllR");
+//        for(int i = 0; i<20 ;i++)
+//        {
+//            DetMCTS_ClassicE_SmartR one = new DetMCTS_ClassicE_SmartR(1,null);
+//            DetMCTS_SmartE_AllR two = new DetMCTS_SmartE_AllR(2,one);
+//            one.setEnemy(two);
+//            new GameSession(one, two).run();
+//        }
+//
+//
+//        Output.getInstance().writeToFile("Player One:DetMCTS_ClassicE_SmartR || Player Two: DetMCTS_SmartE_SmartR");
+//        for(int i = 0; i<20 ;i++)
+//        {
+//            DetMCTS_ClassicE_SmartR one = new DetMCTS_ClassicE_SmartR(1,null);
+//            DetMCTS_SmartE_SmartR two = new DetMCTS_SmartE_SmartR(2,one);
+//            one.setEnemy(two);
+//            new GameSession(one, two).run();
+//        }
+//
+//        Output.getInstance().writeToFile("Player One:DetMCTS_SmartE_SmartR || Player Two: DetMCTS_SmartE_AllR");
+//        for(int i = 0; i<20 ;i++)
+//        {
+//            DetMCTS_SmartE_SmartR one = new DetMCTS_SmartE_SmartR(1,null);
+//            DetMCTS_SmartE_AllR two = new DetMCTS_SmartE_AllR(2,one);
+//            one.setEnemy(two);
+//            new GameSession(one, two).run();
+//        }
 
-        Output.getInstance().writeToFile("Player One:DetMCTS_ClassicE_SmartR || Player Two: DetMCTS_SmartE_AllR");
+
+/**
+ * Standard MCTS testing
+ */
+
+//===========Var1 vs all
+        Output.getInstance().writeToFile("Classic_Expansion_AllRoot || Player Two: Classic_Expansion_SmartRoot");
         for(int i = 0; i<20 ;i++)
         {
-            DetMCTS_ClassicE_SmartR one = new DetMCTS_ClassicE_SmartR(1,null);
-            DetMCTS_SmartE_AllR two = new DetMCTS_SmartE_AllR(2,one);
+            Classic_Expansion_AllRoot one = new Classic_Expansion_AllRoot(1,null);
+            Classic_Expansion_SmartRoot two = new Classic_Expansion_SmartRoot(2,one);
             one.setEnemy(two);
             new GameSession(one, two).run();
         }
 
-
-        Output.getInstance().writeToFile("Player One:DetMCTS_ClassicE_SmartR || Player Two: DetMCTS_SmartE_SmartR");
+        Output.getInstance().writeToFile("Classic_Expansion_AllRoot || Player Two: Rnd_SmartClassic_SmartRoots");
         for(int i = 0; i<20 ;i++)
         {
-            DetMCTS_ClassicE_SmartR one = new DetMCTS_ClassicE_SmartR(1,null);
-            DetMCTS_SmartE_SmartR two = new DetMCTS_SmartE_SmartR(2,one);
+            Classic_Expansion_AllRoot one = new Classic_Expansion_AllRoot(1,null);
+            Rnd_SmartClassic_SmartRoot two = new Rnd_SmartClassic_SmartRoot(2,one);
             one.setEnemy(two);
             new GameSession(one, two).run();
         }
 
-        Output.getInstance().writeToFile("Player One:DetMCTS_SmartE_SmartR || Player Two: DetMCTS_SmartE_AllR");
+        Output.getInstance().writeToFile("Classic_Expansion_AllRoot || Player Two: Rnd_SmartRoot");
         for(int i = 0; i<20 ;i++)
         {
-            DetMCTS_SmartE_SmartR one = new DetMCTS_SmartE_SmartR(2,null);
-            DetMCTS_SmartE_AllR two = new DetMCTS_SmartE_AllR(2,one);
+            Classic_Expansion_AllRoot one = new Classic_Expansion_AllRoot(1,null);
+            Rnd_SmartRoot two = new Rnd_SmartRoot(2,one);
             one.setEnemy(two);
             new GameSession(one, two).run();
         }
-    }
+
+        Output.getInstance().writeToFile("Classic_Expansion_AllRoot || Player Two: SmartExpansion_SmartRoot");
+        for(int i = 0; i<20 ;i++)
+        {
+            Classic_Expansion_AllRoot one = new Classic_Expansion_AllRoot(1,null);
+            SmartExpansion_SmartRoot two = new SmartExpansion_SmartRoot(2,one);
+             one.setEnemy(two);
+            new GameSession(one, two).run();
+        }
+//=========Var 1 finished
+
+//=========Var 2 vs all
+       Output.getInstance().writeToFile("Classic_Expansion_SmartRoot || Player Two: Rnd_SmartClassic_SmartRoot");
+        for(int i = 0; i<20 ;i++)
+        {
+            Classic_Expansion_SmartRoot one = new Classic_Expansion_SmartRoot(1,null);
+            Rnd_SmartClassic_SmartRoot two = new Rnd_SmartClassic_SmartRoot(2,one);
+            one.setEnemy(two);
+            new GameSession(one, two).run();
+        }
+
+        Output.getInstance().writeToFile("Classic_Expansion_SmartRoot || Player Two: Rnd_SmartRoot");
+        for(int i = 0; i<20 ;i++)
+        {
+            Classic_Expansion_SmartRoot one = new Classic_Expansion_SmartRoot(1,null);
+            Rnd_SmartRoot two = new Rnd_SmartRoot(2,one);
+            one.setEnemy(two);
+            new GameSession(one, two).run();
+        }
+
+        Output.getInstance().writeToFile("Classic_Expansion_SmartRoot || Player Two: SmartExpansion_SmartRoot");
+        for(int i = 0; i<20 ;i++)
+        {
+            Classic_Expansion_SmartRoot one = new Classic_Expansion_SmartRoot(1,null);
+            SmartExpansion_SmartRoot two = new SmartExpansion_SmartRoot(2,one);
+            one.setEnemy(two);
+            new GameSession(one, two).run();
+        }
+//=========Var 2 finished
+
+//=========Var 3 vs all
+        Output.getInstance().writeToFile("Rnd_SmartClassic_SmartRoot || Player Two: Rnd_SmartRoot");
+        for(int i = 0; i<20 ;i++)
+        {
+            Rnd_SmartClassic_SmartRoot one = new Rnd_SmartClassic_SmartRoot(1,null);
+            Rnd_SmartRoot two = new Rnd_SmartRoot(2,one);
+            one.setEnemy(two);
+            new GameSession(one, two).run();
+        }
+
+        Output.getInstance().writeToFile("Rnd_SmartClassic_SmartRoot || Player Two: SmartExpansion_SmartRoot");
+        for(int i = 0; i<20 ;i++)
+        {
+            Rnd_SmartClassic_SmartRoot one = new Rnd_SmartClassic_SmartRoot(1,null);
+            SmartExpansion_SmartRoot two = new SmartExpansion_SmartRoot(2,one);
+            one.setEnemy(two);
+            new GameSession(one, two).run();
+        }
+//=========Var 3 finished
+        //4 vs 5
+        Output.getInstance().writeToFile("Rnd_SmartRoot || Player Two: SmartExpansion_SmartRoot");
+        for(int i = 0; i<20 ;i++)
+        {
+            Rnd_SmartRoot one = new Rnd_SmartRoot(1,null);
+            SmartExpansion_SmartRoot two = new SmartExpansion_SmartRoot(2,one);
+            one.setEnemy(two);
+            new GameSession(one, two).run();
+        }
+   }
 }
